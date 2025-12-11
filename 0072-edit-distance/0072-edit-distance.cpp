9690765b1cb11442cac02a701a7f4645c6f00706
Farhan Sadeek <farhan@farhansadeek.com>
2025-12-11 15:37:04 -0500

    Time: 8 ms (46.87%), Space: 10.7 MB (90.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int dp[word1.size() + 1][word2.size() + 1];
+        for(int i = 0; i <= word2.size(); i++){
+            dp[0][i] = i;
+        }
         for(int i = 0; i <= word1.size(); i++){
             dp[i][0] = i;
-        }
-        for(int j = 0; j <= word2.size(); j++){
-            dp[0][j] = j;
         }
         for(int i = 1; i <= word1.size(); i++){
             for(int j = 1; j <= word2.size(); j++){
