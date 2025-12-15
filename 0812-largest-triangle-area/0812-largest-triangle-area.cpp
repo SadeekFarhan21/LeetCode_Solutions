0fa6866880add819abf94c08b1d47d784d88a250
Farhan Sadeek <farhan@farhansadeek.com>
2025-12-15 00:09:18 -0500

    Time: 3 ms (38.94%), Space: 10.4 MB (61.61%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.cpp
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cpp
@@ -15,14 +15,14 @@ public:
     double largestTriangleArea(vector<vector<int>>& points) {
         double answer = 0;
         for(int i = 0; i < points.size(); i++){
-            for(int j = 0; j < points.size(); j++){
-                for(int k = 0; k < points.size(); k++){
-                    if(i != j && j != k){
+            for(int j = i + 1; j < points.size(); j++){
+                for(int k = j + 1; k < points.size(); k++){
+                    
                         double area = triangle_area(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1]);
                         if(area > answer){
                             answer = area;
                         }
-                    }
+                    
                 }
             }
         }
